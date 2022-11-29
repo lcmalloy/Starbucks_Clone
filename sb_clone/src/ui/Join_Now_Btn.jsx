cabbea7d41d7eba06434d03828b5497b28f11d71
Lawrence Malloy <lcmalloy@gmail.com>
2022-11-28 19:42:55 -0800

    added styling to buttons on banner. updated styling for screen resizing.

---
+++ sb_clone/src/ui/Join_Now_Btn.jsx
@@ -0,0 +1,13 @@
+
+
+const Join_Now_Btn = () => {
+  return (
+    <button className="btn-join-now">
+      <a className="join-now-ref" href="https://www.starbucks.com/account/create">
+        Join Now
+      </a>
+    </button>
+  )
+}
+
+export default Join_Now_Btn;
---
+++ sb_clone/src/ui/Sign_In_Btn.jsx
@@ -0,0 +1,13 @@
+import '../styles/Button.css'
+
+const Sign_In_Btn = () => {
+  return (
+    <button className="btn-sign-in">
+      <a className="sign-in-ref" href="https://www.starbucks.com/account/signin?ReturnUrl=%2F">
+        Sign In
+      </a>
+    </button>
+  )
+}
+
+export default Sign_In_Btn;
